4c0fbea6ca9f019666f632cd9cec8bb6bf918507
LuckOfWise <luck.of.wise@gmail.com>
2015-07-31 11:52:14 +0900

    participationの一覧を取れるように

--- lib/remotty/participation.rb
+++ lib/remotty/participation.rb
@@ -1,18 +1,24 @@
 module Remotty
   class Participation
-    attr_accessor :id, :name, :token
+    attr_accessor :id, :name, :icon_url, :token
 
     def initialize(token, attributes = nil)
       self.token = token
       if attributes.present?
         self.id = attributes['id']
         self.name = attributes['name']
+        self.icon_url = attributes['icon_url']
       end
     end
 
     def post_comment(content, show_log = false)
       response = Remotty.access_token(token).post("/api/v1/rooms/participations/#{self.id}/comments.json", body: { comment: { content: content, show_log: show_log } }).parsed
       Remotty::Comment.new(token, response)
+    end
+
+    def self.list(token)
+      response = Remotty.access_token(token).get('/api/v1/rooms.json').parsed
+      Array(response['participations']).map { |attr| Remotty::Participation.new(token, attr) }
     end
   end
 end
